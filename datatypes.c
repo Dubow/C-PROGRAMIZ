2de56282908cd90f32ea952f2b07eebcd32875a7
Abdirahman <abdirahmandubow4@gmail.com>
2025-04-12 20:01:40 +0800

    data types in c

---
+++ datatypes.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main() {
+    // int a = 5;
+    // int b = 10;
+    // int sum = a + b;
+    // printf("The sum is %d\n", sum);
+    // float a = 5.2;
+    // int b = 10.5;
+    // float sum = a + b;
+    // printf("The sum is %.2f\n", sum);
+    // double a = 5;
+    // double b = 10.5;
+    // double sum = a + b;
+    // printf("The sum is %.2lf\n", sum);
+
+    // char a = 'A';
+    // char b = 'B';
+    // printf("character a and b are %c %c", a, b);
+
+// size of bits
+    // int age;
+    // double salary;
+    // char name;
+    // float height;
+    // printf("The size of age is %zu bits\n", sizeof(age));
+    // printf("The size of salary is %zu bits\n", sizeof(salary));
+    // printf("The size of name is %zu bit\n ", sizeof(name));
+    // printf("The size of height is %zu bits\n", sizeof(height));
+
+
+    // get user input
+    // int age;
+    // printf("Please enter your age: ");
+    // scanf("%d", &age);
+    // printf("You age is %d\n", age);
+
+    // remainder operator
+    // int number = 10;
+    // int remainder = number % 3;
+    // printf("The remainder is %d\n", remainder);
+
+    
+    return 0;
+}
